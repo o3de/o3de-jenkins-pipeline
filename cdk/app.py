c5a80959a6eca21699ab47d899d70730fca3c20f
Brian Herrera <brianjherrera@gmail.com>
2023-11-02 12:43:20 -0700

    Add cdk-nag rule set to pipeline and server stacks.
    Signed-off-by: Brian Herrera <brianjherrera@gmail.com>

--- cdk/app.py
+++ cdk/app.py
@@ -10,6 +10,7 @@ import os
 import sys
 import aws_cdk as cdk
 
+from cdk_nag import AwsSolutionsChecks
 from pipeline import JenkinsPipeline, MissingContextError
 
 # Account and region set by the default AWS profile or one specified with --profile
@@ -17,6 +18,7 @@ ACCOUNT = os.environ.get('CDK_DEFAULT_ACCOUNT')
 REGION = os.environ.get('CDK_DEFAULT_REGION')
 
 app = cdk.App()
+cdk.Aspects.of(app).add(AwsSolutionsChecks(verbose=True))
 
 try:
     JenkinsPipeline(app, 'JenkinsPipelineStack', env=cdk.Environment(account=ACCOUNT, region=REGION))
